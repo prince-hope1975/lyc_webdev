075497c4c76f292fc69ce5e301fcfd65804ec09c
prince-hope1975 <amachreeprince7@gmail.com>
2022-08-04 15:38:59 +0100

    ready for build

---
+++ pages/details/submit.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const submit = () => {
+  return (
+    <div style={{width: "100vw",height:"100vh", display:"grid", placeContent:"center", fontSize:"2rem"}}>submit Your details here</div>
+  )
+}
+
+export default submit
