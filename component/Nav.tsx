c1b4e7b3a18d48dd3936d009950dc50331873d83
Prince <amachreeprince7@gmail.com>
2022-08-22 16:53:11 +0100

    Framer

--- component/Nav.tsx
+++ component/Nav.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import { motion, Variants } from "framer-motion";
+import { FaDiscord,  } from "react-icons/fa";
+import {ImTwitter} from "react-icons/im"
+
 const Nav = () => {
   const variant: Variants = {
     initial: {
@@ -12,12 +15,11 @@ const Nav = () => {
     animate: {
       y: 0,
       opacity: 1,
-   
     },
   };
   return (
-    <motion.nav 
-      transition={{ duration: .5, delay:.2 }}
+    <motion.nav
+      transition={{ duration: 0.5, delay: 0.2 }}
       variants={variant}
       animate="animate"
       initial="initial"
@@ -27,8 +29,8 @@ const Nav = () => {
         <Image src={"/logo.svg"} height={100} width={100} />
       </div>
       <div className={styles.main_links_logo}>
-        <Image src={"/discord.svg"} height={100} width={100} />
-        <Image src={"/twitter.svg"} height={100} width={100} />
+        <ImTwitter />
+        <FaDiscord />
       </div>
     </motion.nav>
   );
